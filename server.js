8dae54ae29c05a7c55eb5b3a1e9ec616f4d6eff5
Marco Flores <marcoflores1029@gmail.com>
2022-02-02 15:57:22 -0800

    added the inquirer function from previous repo that had a problem

--- server.js
+++ server.js
@@ -11,3 +11,51 @@ const db = mysql.createConnection({
   password: "password",
   database: "employee_db",
 });
+
+function mainMenu() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "mainmenu",
+          message: "Select Choice",
+          choices: [
+            "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add Department",
+            "Add a Role",
+            "Add an Employee",
+            "Update Employee Role"
+          ],
+        },
+      ])
+      .then((answers) => {
+        // print user choice
+        switch (answers.options) {
+          case "View All Departments":
+            // run function
+            viewDepartment
+            break;
+          case "View All Roles":
+            // run function
+            break;
+          case "View All Employees":
+            // run function
+            break;
+          case "Add Department":
+            // run function
+            break;
+          case "Add a Role":
+            // run function
+            break;
+          case "Add an Employee":
+            // run function
+            break;
+          case "Update Employee Role":
+            // run function
+            break;
+        }
+        console.log(answers.department);
+      });
+  }
