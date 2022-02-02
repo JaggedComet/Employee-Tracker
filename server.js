34eff271bd8f04400cdaf4d15bea73b34055695a
Marco Flores <marcoflores1029@gmail.com>
2022-02-02 15:58:22 -0800

    added working function to view department from previous repo that i had to delete

--- server.js
+++ server.js
@@ -4,58 +4,77 @@ const mysql = require("mysql2");
 const cTable = require('console.table');
 
 const db = mysql.createConnection({
-  host: "localhost",
-  // MySQL username,
-  user: "root",
-  // MySQL password
-  password: "password",
-  database: "employee_db",
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "password",
+    database: "employee_db",
 });
 
 function mainMenu() {
     inquirer
-      .prompt([
-        {
-          type: "list",
-          name: "mainmenu",
-          message: "Select Choice",
-          choices: [
-            "View All Departments",
-            "View All Roles",
-            "View All Employees",
-            "Add Department",
-            "Add a Role",
-            "Add an Employee",
-            "Update Employee Role"
-          ],
-        },
-      ])
-      .then((answers) => {
-        // print user choice
-        switch (answers.options) {
-          case "View All Departments":
-            // run function
-            viewDepartment
-            break;
-          case "View All Roles":
-            // run function
-            break;
-          case "View All Employees":
-            // run function
-            break;
-          case "Add Department":
-            // run function
-            break;
-          case "Add a Role":
-            // run function
-            break;
-          case "Add an Employee":
-            // run function
-            break;
-          case "Update Employee Role":
-            // run function
-            break;
-        }
-        console.log(answers.department);
-      });
-  }
+        .prompt([
+            {
+                type: "list",
+                name: "mainmenu",
+                message: "Select Choice",
+                choices: [
+                    "View All Departments",
+                    "View All Roles",
+                    "View All Employees",
+                    "Add Department",
+                    "Add a Role",
+                    "Add an Employee",
+                    "Update Employee Role"
+                ],
+            },
+        ])
+        .then((answers) => {
+            // print user choice
+            switch (answers.options) {
+                case "View All Departments":
+                    // run function
+                    viewDepartment
+                    break;
+                case "View All Roles":
+                    // run function
+                    break;
+                case "View All Employees":
+                    // run function
+                    break;
+                case "Add Department":
+                    // run function
+                    break;
+                case "Add a Role":
+                    // run function
+                    break;
+                case "Add an Employee":
+                    // run function
+                    break;
+                case "Update Employee Role":
+                    // run function
+                    break;
+            }
+            console.log(answers.department);
+        });
+}
+
+function viewDepartment() {
+    db.query('SELECT * FROM employee', function (err, results) {
+        const choices = results.map((employee) => ({
+            name: `${employee.first_name} ${employee.last_name}`,
+            id: `${employee.id}`
+        }));
+        console.table(choices);
+        mainMenu();
+    })
+}
+
+// db.query('SELECT * FROM employee', function (err, results) {
+//   const choices = results.map((employee) => ({
+//     name: `${employee.first_name} ${employee.last_name}`, 
+//     id: `${employee.id}`
+//   }));
+//   console.table(choices);
+// })
